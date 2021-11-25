2d8eae632cd420497e3549e5a7a67815306a9d11
snowapril <sinjihng@gmail.com>
2021-11-25 10:32:53 +0900

    Add initial main sources

---
+++ target/Sources/main.c
@@ -0,0 +1,17 @@
+#include "stm32f10x.h"
+#include "stm32f10x_exti.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_rcc.h"
+#include "lcd.h"
+#include "touch.h"
+#include "misc.h"
+
+int main(void)
+{
+    SystemInit();
+    
+    // Do stuffs here
+    
+    return 0;
+}
