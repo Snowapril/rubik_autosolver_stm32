01e723af95a09f9798576c1509b79f78ec4d3dd6
blisssm <85685999+blisssm@users.noreply.github.com>
2021-11-28 20:04:38 +0900

    Update pressure.h

--- target/Modules/pressure.h
+++ target/Modules/pressure.h
@@ -1,4 +1,16 @@
 #ifndef PRESSURE_H
 #define PRESSURE_H
 
+#include "stm32f10x_adc.h"
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "misc.h"
+
+void clock_enable();
+void adc_configuration();
+void gpio_configuration();
+void ADC1_2_IRQHandler();
+void NVIC_Configure();
+void get_value();
+
 #endif
