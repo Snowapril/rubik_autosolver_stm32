db76bd6c70da4dcce902e818075b2e2a0f52e5e7
blisssm <85685999+blisssm@users.noreply.github.com>
2021-11-28 20:03:39 +0900

    Update relay.h

--- target/Modules/relay.h
+++ target/Modules/relay.h
@@ -1,4 +1,18 @@
 #ifndef RELAY_H
 #define RELAY_H
 
+#include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+
+typedef struct {
+  unsigned int pin;
+  unsigned int set;
+  unsigned int reset;
+} relay;
+
+void relay_clock_enable();
+void relay_gpio_configuration(relay* R);
+void relay_on(relay* R);
+void relay_off(relay* R);
+
 #endif
