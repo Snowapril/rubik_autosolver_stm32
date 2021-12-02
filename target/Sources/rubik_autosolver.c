6bd7a3db8735889a1b9c7fe12c01c379011ecc1c
snowapril <sinjihng@gmail.com>
2021-12-02 22:52:09 +0900

    Add each module setup code & Fix GPIO pin variable misspell

--- target/Sources/rubik_autosolver.c
+++ target/Sources/rubik_autosolver.c
@@ -2,6 +2,9 @@
 #include "stm32f10x.h"
 #include "stm32f10x_rcc.h"
 #include "system_stm32f10x.h"
+#include "bluetooth.h"
+#include "relay.h"
+#include "pressure.h"
 
 void init_autosolver(struct SolverConfig* config) {
     // main system initialization
@@ -9,20 +12,47 @@ void init_autosolver(struct SolverConfig* config) {
 
     // common clock enable
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+    
+    // initialize each modules
+    // 1. init motor driver
+    //struct MotorDriverConfig motor_config = get_motor_default_config();
+    //motor_driver_init(&motor_config);
+    // 2. init bluetooth module
+    BlueConfig blue_config;
+    Bluetooth_Init(&blue_config);
+    // 3. init relay module
+    //relay relay_config = {
+    //  .pin = GPIO_Pin_3,
+    //  .set = GPIO_BSRR_BS8,
+    //  .reset = GPIO_BSRR_BR8,
+    //};
+    //relay_clock_enable();
+    //relay_gpio_configuration(&relay_config);
+    // 4. init pressure module
+    
 }
 
 int get_autosolver_should_close() {
   return RAS_FALSE;
 }
 
-struct MotorDriverConfig get_motor_default_config() {
-  return struct MotorDriverConfig {
+// <  Motor Driver Config  >
+//  GPIO Clock  : APB2_GPIOD
+//  Common GPIO : GPIOD
+//  Step Pin    : Pin 3
+//  Enable Pin  : Pin 4
+//  Dir Pin     : Pin 5
+//  Timer Clock : APB1_TIM4
+//  Timer Pin   : TIM4
+static struct MotorDriverConfig get_motor_default_config() {
+   static struct MotorDriverConfig config = {
     .gpio_clock_port = RCC_APB2Periph_GPIOD,
     .common_gpio_port = GPIOD,
-    .step_pin = GPIO_PIN_3,
-    .enable_pin = GPIO_PIN_4,
-    .dir_pin = GPIO_PIN5,
+    .step_pin = GPIO_Pin_3,
+    .enable_pin = GPIO_Pin_4,
+    .dir_pin = GPIO_Pin_5,
     .timer_clock_port = RCC_APB1Periph_TIM4,
     .timer_pin = TIM4,
   };
+  return config;
 }
