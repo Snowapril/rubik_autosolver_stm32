5f9ef8f519b1e35ff3f4379963323c9a08db2f5d
blisssm <85685999+blisssm@users.noreply.github.com>
2021-12-02 19:28:22 +0900

    Update pressure.h

--- target/Modules/pressure.h
+++ target/Modules/pressure.h
@@ -6,6 +6,11 @@
 #include "stm32f10x_gpio.h"
 #include "misc.h"
 
+typedef struct{
+  uint16_t pin;/*GPIO_Pin_0*/
+  uint8_t channel;/*ADC_Channel_10 */
+} pressure
+
 void clock_enable();
 void adc_configuration();
 void gpio_configuration();
