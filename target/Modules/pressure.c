a83f808d8ef3aa91f7eb71c2638794d1628b50de
blisssm <85685999+blisssm@users.noreply.github.com>
2021-12-06 00:12:49 +0900

    Update pressure.c

--- target/Modules/pressure.c
+++ target/Modules/pressure.c
@@ -1,13 +1,11 @@
 #include "pressure.h"
 
-uint16_t value = 0;
-
 void clock_enable(){
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 }
 
-void adc_configuration(struct pressure* P){
+void pressure_configuration(struct pressure* P){
   ADC_InitTypeDef adc;
 
   adc.ADC_Mode = ADC_Mode_Independent;
@@ -25,15 +23,14 @@ void adc_configuration(struct pressure* P){
   ADC_StartCalibration(ADC1);
   while (ADC_GetCalibrationStatus(ADC1));
   ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-}
-
-void gpio_configuration(struct pressure* P){
+  
   GPIO_InitTypeDef pressure_GPIO_InitStructure;
   pressure_GPIO_InitStructure.GPIO_Pin = P->pin;
   pressure_GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   pressure_GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
   GPIO_Init(GPIOC, &pressure_GPIO_InitStructure);
 }
+
 
 void ADC1_2_IRQHandler(){
   if(ADC_GetITStatus(ADC1,ADC_IT_EOC)!=RESET) value = ADC_GetConversionValue(ADC1);
@@ -50,8 +47,12 @@ void NVIC_Configure(){
   NVIC_Init(&nvic);
 }
 
-uint16_t get_adc_value(){
+void get_value(){
   ADC_SoftwareStartConvCmd(ADC1, ENABLE);
-  uint16_t value = ADC_GetConversionValue(ADC1);
+  value = ADC_GetConversionValue(ADC1);
   return value;
+}
+
+bool check(){
+  if get_value() >= WEIGHT return true;
 }
