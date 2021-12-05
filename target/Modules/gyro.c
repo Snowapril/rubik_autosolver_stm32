721399cac36488dccbd305ea6e393053636a172e
mingijun <80205593+JUNMinGi@users.noreply.github.com>
2021-12-05 18:21:13 +0900

    Add gyro_start()

--- target/Modules/gyro.c
+++ target/Modules/gyro.c
@@ -1,36 +1,153 @@
 #include "gyro.h"
-#include "stm32f10x_i2c.h"
+
+
+/*
+gyro_start();      <- 여기 함수 다 넣음.
+
+*/
+void delay(){
+	int i;
+	for(i = 0; i < 100000; i++){ }
+}
+
 void gyro_RCC_Configuration() {
+  
   //GPIOB clock enable
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
   //I2C2 clock enable
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
 }
 
-void gyro_GPIO_Configuration(struct GyroConfig* config) {
+void gyro_GPIO_Configuration() {
   GPIO_InitTypeDef gyro_GPIO_InitStructure;
-  //SCL SDA configuration I2C2 using, SCL = PB10, SDA = PB11
-  gyro_GPIO_InitStructure.GPIO_Pin = config->SCL_pin | config->SDA_pin;
+  //SCL SDA configuration I2C2 using, SCL = PB6, SDA = PB7
+  gyro_GPIO_InitStructure.GPIO_Pin = I2C_SCL_pin | I2C_SDA_pin;
   gyro_GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
   gyro_GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;
   GPIO_Init(GPIOB, &gyro_GPIO_InitStructure);
 }
 
-void gyro_I2C2_Configuration() {
+void gyro_I2C1_Configuration() {
   I2C_InitTypeDef  gyro_I2C_InitStructure;
-  I2C_Cmd(I2C2,ENABLE);
+  I2C_Cmd(I2C1,DISABLE); // 초기화
+  //I2C_Cmd(I2C1,ENABLE);
   gyro_I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
   gyro_I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
-  gyro_I2C_InitStructure.I2C_OwnAddress1 = I2C2_SLAVE1_ADDRESS7;
-  gyro_I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+  gyro_I2C_InitStructure.I2C_OwnAddress1 = I2C1_SLAVE_ADDRESS7;
+  gyro_I2C_InitStructure.I2C_Ack = I2C_Ack_Disable;
   gyro_I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-  gyro_I2C_InitStructure.I2C_ClockSpeed = ClockSpeed;
-  I2C_Init(I2C2, &gyro_I2C_InitStructure);
+  gyro_I2C_InitStructure.I2C_ClockSpeed = Gyro_ClockSpeed;
+  I2C_Init(I2C1, &gyro_I2C_InitStructure);
+  I2C_Cmd(I2C1,ENABLE);
 }
 
+void I2C_start_Tx(I2C_TypeDef* I2Cx,uint8_t address, uint8_t direction) {
+  while(I2C_GetFlagStatus(I2Cx,I2C_FLAG_BUSY));
+  I2C_GenerateSTART(I2Cx,ENABLE);
+  while(!I2C_CheckEvent(I2Cx,I2C_EVENT_MASTER_MODE_SELECT));
+  I2C_Send7bitAddress(I2Cx,address,direction);
+  while(!I2C_CheckEvent(I2Cx,I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+}
+
+void I2C_start_Rx(I2C_TypeDef* I2Cx,uint8_t address,uint8_t direction) {
+  I2C_GenerateSTART(I2Cx,ENABLE);
+  while(!I2C_CheckEvent(I2Cx,I2C_EVENT_MASTER_MODE_SELECT));
+  I2C_Send7bitAddress(I2Cx,address,direction);
+  while(!I2C_CheckEvent(I2Cx,I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+}
+
+void I2C_write(I2C_TypeDef* I2Cx, uint8_t data) {
+  I2C_SendData(I2Cx,data);
+  while(!I2C_CheckEvent(I2Cx,I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+}
+
+uint8_t I2C_read_ack(I2C_TypeDef* I2Cx) {
+  I2C_AcknowledgeConfig(I2Cx,ENABLE);
+  while(!I2C_CheckEvent(I2Cx,I2C_EVENT_MASTER_BYTE_RECEIVED));
+  uint8_t data = I2C_ReceiveData(I2Cx);
+  return data;
+}
+uint8_t I2C_read_nack(I2C_TypeDef* I2Cx) {
+  I2C_AcknowledgeConfig(I2Cx,DISABLE);
+  I2C_GenerateSTOP(I2Cx,ENABLE);
+  while(!I2C_CheckEvent(I2Cx,I2C_EVENT_MASTER_BYTE_RECEIVED));
+  uint8_t data = I2C_ReceiveData(I2Cx);
+  return data;
+}
+
+void I2C_stop(I2C_TypeDef* I2Cx) {
+  I2C_GenerateSTOP(I2Cx,ENABLE);
+}
+
+
+uint8_t myMPU6050Read(uint8_t reg) {
+  unsigned char data = 0;
+  I2C_start_Tx(I2C1,I2C1_SLAVE_ADDRESS7 << 1, I2C_Direction_Transmitter);
+  I2C_write(I2C1, reg);
+  I2C_start_Rx(I2C1, I2C1_SLAVE_ADDRESS7 << 1, I2C_Direction_Receiver);
+  data = I2C_read_nack(I2C1);
+  I2C_stop(I2C1);
+  return data;
+} // 여기 고쳐야 할수도
+
+float C_Accel_Z() {
+  uint16_t HighBit,LowBit,RawData;
+  float data;
+  HighBit = myMPU6050Read(MPU_ADDR);
+  LowBit = myMPU6050Read(MPU_ADDR+1);
+  RawData = HighBit << 8 | LowBit;
+  data = (float)RawData; // raw data 받기
+  return data;
+} // 여기 고쳐야 할수도
+
+void gyro_start() {
+  gyro_RCC_Configuration();
+  gyro_GPIO_Configuration();
+  gyro_I2C1_Configuration();
+  float AccelData;
+  while(1) {
+    AccelData = C_Accel_Z();
+    printf("AccelData = %f\n",AccelData);
+  }
+}
+
+
+
+
+
+
+/*
+void I2C_SET() {
+  I2C1->CR1 = CR1_PE_Set;
+}
+void I2C_RESET() {
+  I2C1->CR1 = CR1_PE_Reset;
+}
 
 
 void gyro_READ_DATA() {
-  
+  I2C_GenerateSTART(I2C1,ENABLE);
+  while(!I2C_CheckEvent(I2C1, I2C_EVENT_MASTER_MODE_SELECT));
+  I2C_Send7bitAddress(I2C1, I2C2_SLAVE1_ADDRESS7, I2C_Direction_Transmitter);
+  while(1) {
+    if(I2C_CheckEvent(I2C1,I2C_EVENT_MASTER_BYTE_RECEIVED)) {
+      Gyro_Data[countx] = I2C_ReceiveData(I2C1);
+      countx++;
+    }
+    delay();
+    if(countx == 7) {
+      break;
+    }
+  }
 }
 
+void gyro_PRINT_DATA() {
+  int i = 0;
+  printf("Gyro_Data : ");
+  for(i = 0; i < 8; i++) {
+    printf("%c ",Gyro_Data[i]);
+  }
+  printf("\n");
+  
+}
+*/
--- target/Modules/gyro.h
+++ target/Modules/gyro.h
@@ -3,30 +3,50 @@
 
 #include "stm32f10x.h"
 #include "stm32f10x_i2c.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// mpu6050의 i2c 주소값은 0x68
 
 #define I2C1_SLAVE_ADDRESS7    0x30
 #define I2C2_SLAVE1_ADDRESS7   0x30
-#define BufferSize             4 
-#define ClockSpeed             200000
+#define Gyro_BufferSize             4 
+#define Gyro_ClockSpeed             200000
+#define CR1_PE_Set              ((u16)0x0001)
+#define CR1_PE_Reset            ((u16)0xFFFE)
+#define I2C_SCL_pin GPIO_Pin_6
+#define I2C_SDA_pin GPIO_Pin_7
 
+#define MPU_ADDR 0x68
 /*
 GYRO
 GPIOB
-SCL = PB10
-SDA = PB11
+SCL = PB6
+SDA = PB7
 
 */
 
+char Gyro_Data[8];
+uint8_t countx=0;
 
-struct GyroConfig {
-    GPIO_TypeDef* common_gpio_port;
-    unsigned int SCL_pin = GPIO_Pin_10;
-    unsigned int SDA_pin = GPIO_Pin_11;
-};
+void delay();
 
 void gyro_RCC_Configuration();
 void gyro_GPIO_Configuration();
-void gyro_I2C2_Configuration();
+void gyro_I2C1_Configuration();
+
+// https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=alsrb968&logNo=220733739953
+// iic 통신 으로 가속도 센서 읽기 AHRS모듈 이용 이것을 수정하자. si
+// single_byte read 이용
+void I2C_start_Tx(I2C_TypeDef* I2Cx,uint8_t address, uint8_t direction);
+void I2C_start_Rx(I2C_TypeDef* I2Cx,uint8_t address,uint8_t direction);
+void I2C_write(I2C_TypeDef* I2Cx, uint8_t data);
+uint8_t I2C_read_ack(I2C_TypeDef* I2Cx);
+uint8_t I2C_read_nack(I2C_TypeDef* I2Cx);
+void I2C_stop(I2C_TypeDef* I2Cx);
+uint8_t myMPU6050Read(uint8_t reg);
+float C_Accel_Z();
+void gyro_start();
 
 
 #endif
