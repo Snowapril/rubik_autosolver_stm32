03d8add9a5026cffc3b5ee8715c73b8bf12b4a97
snowapril <sinjihng@gmail.com>
2021-12-17 19:42:00 +0900

    Fix bluetooth module

--- target/Modules/bluetooth.c
+++ target/Modules/bluetooth.c
@@ -3,13 +3,24 @@
 
 //RCC initiate for Bluetooth
 static void Bluetooth_RCC_Init(){
-    RCC_APB2PeriphClockCmd((RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO | RCC_APB2Periph_USART1) , ENABLE);
+    RCC_APB2PeriphClockCmd((RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO | RCC_APB2Periph_USART1) , ENABLE);
     RCC_APB1PeriphClockCmd((RCC_APB1Periph_USART2) , ENABLE);
 }
 
 // GPIO initiate for Bluetooth
 static void Bluetooth_GPIO_Init(){
     GPIO_InitTypeDef Gpio_Init;
+    // USART1 output Mode
+    Gpio_Init.GPIO_Mode = GPIO_Mode_AF_PP;
+    Gpio_Init.GPIO_Pin=(GPIO_Pin_9);
+    Gpio_Init.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &Gpio_Init);
+
+    //USART1 input Mode
+    Gpio_Init.GPIO_Mode = GPIO_Mode_IPD;
+    Gpio_Init.GPIO_Pin=(GPIO_Pin_10);
+    GPIO_Init(GPIOA, &Gpio_Init);
+    
     Gpio_Init.GPIO_Pin   = GPIO_Pin_2;
     Gpio_Init.GPIO_Speed = GPIO_Speed_50MHz;
     Gpio_Init.GPIO_Mode  = GPIO_Mode_AF_PP;
@@ -24,32 +35,33 @@ static void Bluetooth_GPIO_Init(){
 }
 
 static void Bluetooth_USART_Init(){
-    USART_InitTypeDef Usart_Init;
+    // set usart
+    USART_InitTypeDef usart1;
+    USART_InitTypeDef usart2;
 
-    //setting for debug using the PC (USART1)
-    Usart_Init.USART_BaudRate            = 115200;
-    Usart_Init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    Usart_Init.USART_Mode                = (USART_Mode_Rx|USART_Mode_Tx);
-    Usart_Init.USART_Parity              = USART_Parity_No;
-    Usart_Init.USART_StopBits            = USART_StopBits_1;
-    Usart_Init.USART_WordLength          = USART_WordLength_8b;
-    
-    USART_Init(USART1, &Usart_Init);
-    
+    usart1.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    usart1.USART_BaudRate = 9600; // USART1
+    usart1.USART_WordLength = USART_WordLength_8b;
+    usart1.USART_StopBits=USART_StopBits_1;
+    usart1.USART_Parity=USART_Parity_No;
+    usart1.USART_Mode = (USART_Mode_Rx | USART_Mode_Tx);
+
+    usart2.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    usart2.USART_BaudRate = 9600; // USART2
+    usart2.USART_WordLength = USART_WordLength_8b;
+    usart2.USART_StopBits=USART_StopBits_1;
+    usart2.USART_Parity=USART_Parity_No;
+    usart2.USART_Mode = (USART_Mode_Rx | USART_Mode_Tx);
+
+    USART_Init(USART1, &usart1);
     USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
-    USART_Cmd(USART1, ENABLE);
 
-    //setting for send and receive data from bluetooth (USART2)
-    Usart_Init.USART_BaudRate            = 9600;
-    Usart_Init.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    Usart_Init.USART_Mode                = (USART_Mode_Rx|USART_Mode_Tx);
-    Usart_Init.USART_Parity              = USART_Parity_No;
-    Usart_Init.USART_StopBits            = USART_StopBits_1;
-    Usart_Init.USART_WordLength          = USART_WordLength_8b;
-    
-    USART_Init(USART2, &Usart_Init);
-
+    USART_Init(USART2, &usart2);
     USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+
+    EXTI_ClearITPendingBit(USART_IT_RXNE);
+
+    USART_Cmd(USART1, ENABLE);
     USART_Cmd(USART2, ENABLE);
 }
 
@@ -63,8 +75,8 @@ static void Bluetooth_NVIC_Init(){
     Nvic_Init.NVIC_IRQChannelPreemptionPriority = 1;
     Nvic_Init.NVIC_IRQChannelSubPriority        = 1;
     Nvic_Init.NVIC_IRQChannelCmd                = ENABLE;
-    
-	NVIC_Init(&Nvic_Init);
+     
+    NVIC_Init(&Nvic_Init);
 
     // Bluetooth interrupt
     Nvic_Init.NVIC_IRQChannel                   = USART2_IRQn;
@@ -72,7 +84,7 @@ static void Bluetooth_NVIC_Init(){
     Nvic_Init.NVIC_IRQChannelSubPriority        = 1;
     Nvic_Init.NVIC_IRQChannelCmd                = ENABLE;
     
-	NVIC_Init(&Nvic_Init);
+    NVIC_Init(&Nvic_Init);
 }
 
 void Bluetooth_Init(BlueConfig *config){
--- target/Sources/main.c
+++ target/Sources/main.c
@@ -4,32 +4,20 @@
 #include "Common/utils.h"
 #include <stdbool.h>
 
-BlueConfig blue;
+void USART1_IRQHandler() { // 컴퓨터 키보드 입력시
+	if (USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET) {
+		printf("Keyboard input");
+	}
 
-/// IRQHandler Setting
-void USART1_IRQHandler(){
-    unsigned char recvbuf = '\0';
-
-    while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
-        ; // Wait the USART1 Tx to SET
-    recvbuf = (unsigned char)USART_ReceiveData(USART1);
-    USART_SendData(USART1, recvbuf); // ECHO
-    USART_SendData(USART2, recvbuf); // Send to bluetooth
-
-    USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+	USART_ClearITPendingBit(USART1, USART_IT_RXNE);
 }
 
-void USART2_IRQHandler(){
-    unsigned char recvbuf = '\0';
+void USART2_IRQHandler() { // 블루투스 입력시
+	if (USART_GetITStatus(USART2,USART_IT_RXNE)!=RESET) {
+          printf("Bluetooth input");
+	}
 
-    while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET)
-        ; // Wait the USART2 Tx to SET
-    recvbuf = (unsigned char)USART_ReceiveData(USART2); // ECHO
-    USART_SendData(USART1, recvbuf); // Send to bluetooth
-
-    blue.isValid = true;
-
-    USART_ClearITPendingBit(USART2, USART_IT_RXNE);
+	USART_ClearITPendingBit(USART2, USART_IT_RXNE);
 }
 
 int main(void) {
@@ -40,15 +28,16 @@ int main(void) {
     // initialize rubik cube auto solver
     init_autosolver(&config);
     
-    Bluetooth_Init(&blue);
-    if(!blue.isATscan && Bluetooth_send_data("AT+BTSCAN", &blue)){
-        blue.isATscan = true;
-        delay();
-    }
-
     while (get_autosolver_should_close() !=  RAS_TRUE) {
         // Do stuff
+        
+      GPIO_SetBits(GPIOD, GPIO_Pin_3);
+      delay();
+      GPIO_ResetBits(GPIOD, GPIO_Pin_3);
+      delay();
     }
+ 
+    
     
     return 0;
 }
