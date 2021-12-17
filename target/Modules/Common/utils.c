5677868097749c9a5a66bef4af2a8de7339dbea6
snowapril <sinjihng@gmail.com>
2021-12-17 15:34:13 +0900

    Add delay_ms and delay_us for control

--- target/Modules/Common/utils.c
+++ target/Modules/Common/utils.c
@@ -4,3 +4,28 @@ void delay(){
 	int i;
 	for(i = 0; i < 100000; i++){ }
 }
+
+// delay functions from https://github.com/BlaCkinkGJ/STM32F107-Hardware-Term
+void delay_ms(unsigned int msec){
+    uint32_t temp;
+    SysTick->LOAD = (uint32_t)msec*(SystemCoreClock/8/1000);
+    SysTick->VAL  = 0x00;        // clear Count flag
+    SysTick->CTRL = 0x01;
+    do{
+        temp=SysTick->CTRL;
+    }while((temp&0x01)&&!(temp&(1<<16)));    // wait Count flag set
+    SysTick->CTRL = 0x00;
+    SysTick->VAL  = 0X00;
+}
+
+void delay_us(unsigned int usec){
+    uint32_t temp;
+    SysTick->LOAD = (uint32_t)usec*(SystemCoreClock/8/1000000);
+    SysTick->VAL  = 0x00;        // clear Count flag
+    SysTick->CTRL = 0x01;
+    do{
+        temp = SysTick->CTRL;
+    }while((temp&0x01)&&!(temp&(1<<16)));    // wait Count flag set
+    SysTick->CTRL = 0x00;
+    SysTick->VAL  = 0X00;
+}
--- target/Modules/Common/utils.h
+++ target/Modules/Common/utils.h
@@ -1,6 +1,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+// temporal delay function, just loop 100,000 times
 void delay();
+// delay for msec milliseconds
+void delay_ms(unsigned int msec);
+// delay for usec microseconds
+void delay_us(unsigned int usec);
 
 #endif
