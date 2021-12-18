db1edda7f642948d60403d2ee4122f2d62d3461d
mingijun <80205593+JUNMinGi@users.noreply.github.com>
2021-12-18 12:51:21 +0900

    click solve, toast message

--- app/app/src/main/java/com/example/rubiksolver_controller/FinalView.java
+++ app/app/src/main/java/com/example/rubiksolver_controller/FinalView.java
@@ -5,11 +5,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class FinalView extends AppCompatActivity {
 
@@ -29,17 +32,17 @@ public class FinalView extends AppCompatActivity {
         top.add((Button) findViewById(R.id.top31));top.add((Button) findViewById(R.id.top32));top.add((Button) findViewById(R.id.top33));
         buttonsMap.put("top", top);
 
-        ArrayList<Button> front = new ArrayList<>();
-        front.add((Button) findViewById(R.id.front11));front.add((Button) findViewById(R.id.front12));front.add((Button) findViewById(R.id.front13));
-        front.add((Button) findViewById(R.id.front21));front.add((Button) findViewById(R.id.front22));front.add((Button) findViewById(R.id.front23));
-        front.add((Button) findViewById(R.id.front31));front.add((Button) findViewById(R.id.front32));front.add((Button) findViewById(R.id.front33));
-        buttonsMap.put("front", front);
-
         ArrayList<Button> left = new ArrayList<>();
         left.add((Button) findViewById(R.id.left11));left.add((Button) findViewById(R.id.left12));left.add((Button) findViewById(R.id.left13));
         left.add((Button) findViewById(R.id.left21));left.add((Button) findViewById(R.id.left22));left.add((Button) findViewById(R.id.left23));
         left.add((Button) findViewById(R.id.left31));left.add((Button) findViewById(R.id.left32));left.add((Button) findViewById(R.id.left33));
         buttonsMap.put("left", left);
+
+        ArrayList<Button> front = new ArrayList<>();
+        front.add((Button) findViewById(R.id.front11));front.add((Button) findViewById(R.id.front12));front.add((Button) findViewById(R.id.front13));
+        front.add((Button) findViewById(R.id.front21));front.add((Button) findViewById(R.id.front22));front.add((Button) findViewById(R.id.front23));
+        front.add((Button) findViewById(R.id.front31));front.add((Button) findViewById(R.id.front32));front.add((Button) findViewById(R.id.front33));
+        buttonsMap.put("front", front);
 
         ArrayList<Button> right = new ArrayList<>();
         right.add((Button) findViewById(R.id.right11));right.add((Button) findViewById(R.id.right12));right.add((Button) findViewById(R.id.right13));
@@ -47,25 +50,45 @@ public class FinalView extends AppCompatActivity {
         right.add((Button) findViewById(R.id.right31));right.add((Button) findViewById(R.id.right32));right.add((Button) findViewById(R.id.right33));
         buttonsMap.put("right", right);
 
-        ArrayList<Button> bottom = new ArrayList<>();
-        bottom.add((Button) findViewById(R.id.bottom11));bottom.add((Button) findViewById(R.id.bottom12));bottom.add((Button) findViewById(R.id.bottom13));
-        bottom.add((Button) findViewById(R.id.bottom21));bottom.add((Button) findViewById(R.id.bottom22));bottom.add((Button) findViewById(R.id.bottom23));
-        bottom.add((Button) findViewById(R.id.bottom31));bottom.add((Button) findViewById(R.id.bottom32));bottom.add((Button) findViewById(R.id.bottom33));
-        buttonsMap.put("bottom", bottom);
-
         ArrayList<Button> back = new ArrayList<>();
         back.add((Button) findViewById(R.id.back11));back.add((Button) findViewById(R.id.back12));back.add((Button) findViewById(R.id.back13));
         back.add((Button) findViewById(R.id.back21));back.add((Button) findViewById(R.id.back22));back.add((Button) findViewById(R.id.back23));
         back.add((Button) findViewById(R.id.back31));back.add((Button) findViewById(R.id.back32));back.add((Button) findViewById(R.id.back33));
         buttonsMap.put("back", back);
 
+        ArrayList<Button> bottom = new ArrayList<>();
+        bottom.add((Button) findViewById(R.id.bottom11));bottom.add((Button) findViewById(R.id.bottom12));bottom.add((Button) findViewById(R.id.bottom13));
+        bottom.add((Button) findViewById(R.id.bottom21));bottom.add((Button) findViewById(R.id.bottom22));bottom.add((Button) findViewById(R.id.bottom23));
+        bottom.add((Button) findViewById(R.id.bottom31));bottom.add((Button) findViewById(R.id.bottom32));bottom.add((Button) findViewById(R.id.bottom33));
+        buttonsMap.put("bottom", bottom);
+
+
+
         Intent intent = getIntent();
         //this.cube = (HashMap<String, char[][]>)intent.getSerializableExtra("cube");
 
         //read hashmap and color grid
+//        ArrayList<char[]> cubestate = new ArrayList<>();
+//
+//        int cubevalueindex = 6,cubevalueindex2 = 6,cubevalueindex3 = 9;
         for (String key:Storage.cube.keySet()){
             char[][] thisFace = Storage.cube.get(key);
             for (int i=0;i<3;i++){
+//                if(key == "back") {
+//                    cubestate.add(cubevalueindex,thisFace[i]);
+//                    cubevalueindex += 1;
+//                }
+//                else if(key == "front") {
+//                    cubestate.add(cubevalueindex2,thisFace[i]);
+//                    cubevalueindex2 += 1;
+//                }
+//                else if(key == "right") {
+//                    cubestate.add(cubevalueindex3,thisFace[i]);
+//                    cubevalueindex3 += 1;
+//                }
+//                else {
+//                    cubestate.add(thisFace[i]);
+//                }
                 for (int j=0;j<3;j++){
                     int buttonIndex = i*3 + j;
                     Button btn = buttonsMap.get(key).get(buttonIndex);
@@ -94,7 +117,57 @@ public class FinalView extends AppCompatActivity {
                 }
             }
         }
+//        Collections.swap(cubestate,4,7);
+//        Collections.swap(cubestate,5,11);
+//        Collections.swap(cubestate,6,12);
+//        Collections.swap(cubestate,10,13);
+//
+//
+//        for(int k = 0; k < cubestate.size(); k++) {
+//            System.out.println("cubevalues = " + cubestate.get(k)[0] + cubestate.get(k)[1] + cubestate.get(k)[2]);
+//        }
     }
+
+
+    public void ChangeType(View view) {
+        ArrayList<char[]> cubestate = new ArrayList<>();
+        int cubevalueindex = 6,cubevalueindex2 = 6,cubevalueindex3 = 9;
+        for (String key:Storage.cube.keySet()) {
+            char[][] thisFace = Storage.cube.get(key);
+            for (int i = 0; i < 3; i++) {
+                if (key == "back") {
+                    cubestate.add(cubevalueindex, thisFace[i]);
+                    cubevalueindex += 1;
+                } else if (key == "front") {
+                    cubestate.add(cubevalueindex2, thisFace[i]);
+                    cubevalueindex2 += 1;
+                } else if (key == "right") {
+                    cubestate.add(cubevalueindex3, thisFace[i]);
+                    cubevalueindex3 += 1;
+                } else {
+                    cubestate.add(thisFace[i]);
+                }
+            }
+        }
+        Collections.swap(cubestate,4,7);
+        Collections.swap(cubestate,5,11);
+        Collections.swap(cubestate,6,12);
+        Collections.swap(cubestate,10,13);
+        String cubeState_str;
+        cubeState_str = "";
+
+        for(int k = 0; k < cubestate.size(); k++) {
+            //System.out.println("cubevalues = " + cubestate.get(k)[0] + cubestate.get(k)[1] + cubestate.get(k)[2]);
+            cubeState_str = cubeState_str + cubestate.get(k)[0] + cubestate.get(k)[1] + cubestate.get(k)[2];
+            System.out.println("cubeString = " + cubeState_str);
+        }
+        if(view.getId() == R.id.button10) {  // Buttoon의 ID를 찾아서 실행이 된다.
+            Toast.makeText(this, cubeState_str, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 
     public void finalGoTop(View view){
         Intent intent = new Intent(this, com.example.rubiksolver_controller.TopActivity.class);
