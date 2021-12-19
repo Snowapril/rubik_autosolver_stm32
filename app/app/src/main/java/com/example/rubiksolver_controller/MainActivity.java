7993e24b6e9dc8730599da1bc89e19f4c16b7ebc
snowapril <sinjihng@gmail.com>
2021-12-19 23:46:45 +0900

    Implement bluetooth scan and connection

--- app/app/src/main/java/com/example/rubiksolver_controller/MainActivity.java
+++ app/app/src/main/java/com/example/rubiksolver_controller/MainActivity.java
@@ -1,26 +1,47 @@
 package com.example.rubiksolver_controller;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.ActivityCompat;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.HashMap;
-
-//import com.google.android.filament.utils.*;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.view.Choreographer;
-//import android.view.SurfaceView;
-//import android.view.View;
-//import android.widget.Toast;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = "MainActivity";
+    UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
 
-    //HashMap<String,char[][]> cube;
+    BluetoothAdapter btAdapter;
+    Set<BluetoothDevice> pairedDevices;
+    ArrayAdapter<String> btArrayAdapter;
+    ArrayList<String> deviceAddressArray;
+    TextView textStatus;
+    Button btnParied, btnSend;
+    ListView listView;
+    BluetoothSocket btSocket = null;
+    ConnectedThread connectedThread;
+
+    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +56,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CharSequence text = "Cube cleared.";
-        Toast toast=Toast.makeText(this, text, Toast.LENGTH_SHORT);
-        toast.show();
+        textStatus = (TextView) findViewById(R.id.text_status);
+        btnParied = (Button) findViewById(R.id.btn_paired);
+        btnSend = (Button) findViewById(R.id.btn_send);
+        listView = (ListView) findViewById(R.id.listview);
+        listView.setOnItemClickListener(new myOnItemClickListener());
+
+        // Get permission
+        String[] permission_list = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+
+        ActivityCompat.requestPermissions(MainActivity.this, permission_list,  1);
+
+        // Enable bluetooth
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        // show paired devices
+        btArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        deviceAddressArray = new ArrayList<>();
+        listView.setAdapter(btArrayAdapter);
+    }
+
+    public void onClickButtonPaired(View view) {
+        btArrayAdapter.clear();
+        if(deviceAddressArray!=null && !deviceAddressArray.isEmpty()){ deviceAddressArray.clear(); }
+        pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                btArrayAdapter.add(deviceName);
+                deviceAddressArray.add(deviceHardwareAddress);
+            }
+        }
+    }
+
+    public void onClickButtonSend(View view) {
+        if(connectedThread!=null){ connectedThread.write("a"); }
     }
 
     public void onStart(View view){
@@ -45,50 +107,46 @@ public class MainActivity extends AppCompatActivity {
         //intent.putExtra("cube", Storage.cube);
         startActivity(intent);
     }
+
+    public class myOnItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(getApplicationContext(), btArrayAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+
+            textStatus.setText("try...");
+
+            final String name = btArrayAdapter.getItem(position); // get name
+            final String address = deviceAddressArray.get(position); // get address
+            boolean flag = true;
+
+            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+            // create & connect socket
+            try {
+                btSocket = createBluetoothSocket(device);
+                btSocket.connect();
+            } catch (IOException e) {
+                flag = false;
+                textStatus.setText("connection failed!");
+                e.printStackTrace();
+            }
+
+            if(flag){
+                textStatus.setText("connected to "+name);
+                connectedThread = new ConnectedThread(btSocket);
+                connectedThread.start();
+            }
+
+        }
+
+        private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+            try {
+                final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+                return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
+            } catch (Exception e) {
+                Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+            }
+            return  device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
+        }
+    }
 }
-
-
-//public class MainActivity extends AppCompatActivity {
-//
-//    private SurfaceView m_surfaceView;
-//    private Choreographer m_choreographer;
-//    private ModelViewer m_modelViewer;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        m_surfaceView = new SurfaceView(this);
-//        this.setContentView(m_surfaceView);
-//        m_choreographer = Choreographer.getInstance();
-//        m_modelViewer = new ModelViewer(m_surfaceView);
-//        m_surfaceView.setOnTouchListener(m_modelViewer);
-//    }
-//
-//    private Choreographer.FrameCallback m_frameCallback = new Choreographer.FrameCallback() {
-//        @Override
-//        public void doFrame(long currentTime) {
-//            m_choreographer.postFrameCallback(this);
-//            m_modelViewer.render(currentTime);
-//        }
-//    };
-//
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        m_choreographer.postFrameCallback(m_frameCallback);
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        m_choreographer.removeFrameCallback(m_frameCallback);
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        m_choreographer.removeFrameCallback(m_frameCallback);
-//    }
-//}
